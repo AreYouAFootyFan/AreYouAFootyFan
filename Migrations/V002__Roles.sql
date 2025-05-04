fa4b78bffd80fae4b214227287a160c85ec093a5
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-05-04 23:15:03 +0200

    Initial data

---
+++ Migrations/V002__Roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (role_name) VALUES
+    ('Quiz Taker'),
+    ('Quiz Master');
---
+++ Migrations/V005__Difficulty_Levels.sql
@@ -0,0 +1,7 @@
+INSERT INTO difficulty_levels 
+(difficulty_level, time_limit_seconds, points_on_correct, points_on_incorrect)
+VALUES
+    ('Easy', 15, 5, -2),
+    ('Medium', 20, 10, -5),
+    ('Hard', 30, 20, -10),
+    ('Impossible', 40, 40, -20);
