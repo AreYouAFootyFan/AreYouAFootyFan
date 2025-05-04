e358cdfff1fa7116ca5d64ff90aae27dac2c3eed
Tevlen Naidoo <tevlen.naidoo@bbd.co.za>
2025-05-05 00:38:49 +0200

    Database Initialisation (#4)
    * Initial data
    
    * More data

---
+++ Migrations/V003__Categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (category_name, category_description) VALUES
+    ('World Cup', 'FIFA World Cup trivia'),
+    ('Premier League', 'English Premier League facts'),
+    ('Legends', 'Legendary football players');
---
+++ Migrations/V004__Badges.sql
@@ -0,0 +1,6 @@
+INSERT INTO badges (badge_name, minimum_points) VALUES
+    ('Rookie', 50),
+    ('Bench Warmer', 100),
+    ('Midfielder', 200),
+    ('Striker', 500),
+    ('Legend', 1000);
