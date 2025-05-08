6b196a48346fbacbdf3af3238da6a5ecf64e6a68
Alexander van der Leek <Alexander.VanderLeek@bbd.co.za>
2025-05-08 14:40:04 +0200

    Refactor profile page.

--- public/static/js/views/HomeView.js
+++ public/static/js/views/HomeView.js
@@ -1,8 +1,8 @@
 import AbstractView from "./AbstractView.js";
-import "../components/Homepage/QuizHome.js";
-import "../components/Homepage/QuizCard.js";
-import "../components/Homepage/QuizLeaderboard.js";
-import "../components/Homepage/QuizCategoryFilter.js"
+import "../components/homepage/QuizHome.js";
+import "../components/homepage/QuizCard.js";
+import "../components/homepage/QuizLeaderboard.js";
+import "../components/homepage/QuizCategoryFilter.js"
 
 export default class HomeView extends AbstractView {
     constructor() {
