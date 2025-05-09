237c9d33faedd720064846b04f319ba67b41bc81
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-05-09 16:57:32 +0200

    Corrected some max lengths to correspond to the database definition

--- src/utils/enums/length/index.ts
+++ src/utils/enums/length/index.ts
@@ -10,20 +10,20 @@ export enum Min {
   // User-related
   USERNAME = 3,
   PASSWORD = 8,
-  
+
   // Question and answer-related
   QUESTION_TEXT = 10,
   ANSWER_TEXT = 1,
-  
+
   // Category-related
   CATEGORY_NAME = 2,
   CATEGORY_DESCRIPTION = 5,
-  
+
   // Quiz-related
   QUIZ_TITLE = 5,
   QUIZ_DESCRIPTION = 10,
   QUESTIONS_PER_QUIZ = 5,
-  
+
   // Difficulty-related
   DIFFICULTY_NAME = 3,
 }
@@ -33,28 +33,28 @@ export enum Min {
  */
 export enum Max {
   // User-related
-  USERNAME = 32,
+  USERNAME = 16,
   PASSWORD = 64,
-  
+
   // Question and answer-related
   QUESTION_TEXT = 256,
   ANSWER_TEXT = 128,
-  
+
   // Category-related
   CATEGORY_NAME = 32,
   CATEGORY_DESCRIPTION = 64,
-  
+
   // Quiz-related
   QUIZ_TITLE = 64,
-  QUIZ_DESCRIPTION = 256,
+  QUIZ_DESCRIPTION = 128,
   ANSWERS_PER_QUESTION = 4,
-  
+
   // Difficulty-related
   DIFFICULTY_NAME = 16,
-  
+
   // File upload-related
   FILE_SIZE_MB = 5,
-  
+
   // Pagination-related
   PAGE_SIZE = 20,
   DEFAULT_LIMIT = 10,
@@ -68,13 +68,13 @@ export enum Default {
   QUESTIONS_PER_QUIZ = 5,
   ANSWERS_PER_QUESTION = 4,
   CORRECT_ANSWERS_PER_QUESTION = 1,
-  
+
   // Leaderboard-related
   LEADERBOARD_LIMIT = 5,
-  
+
   // Pagination-related
   PAGE_SIZE = 10,
-  
+
   // Time-related (in minutes)
   SESSION_TIMEOUT = 60,
-} 
+}
--- src/utils/enums/message/index.ts
+++ src/utils/enums/message/index.ts
@@ -1,16 +1,8 @@
 /**
  * Re-export all message enums
  */
-import * as Error from './error';
+import * as Error from "./error";
 export { Error };
 
-import * as Success from './success';
+import * as Success from "./success";
 export { Success };
-
-/**
- * Success messages for various operations
- */
-export enum SuccessMessage {
-  ANSWER_DELETE = "Answer deleted successfully",
-  CATEGORY_DELETE = "Category deleted successfully",
-} 
